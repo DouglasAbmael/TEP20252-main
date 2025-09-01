a71482d24a71ed4ced18ad1bf48d6138bf499f76
DouglasAbmael <141178215+DouglasAbmael@users.noreply.github.com>
2025-09-01 17:57:10 -0300

    0109-10(inconcluido)

--- aplicacao/admin.py
+++ aplicacao/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Produto
+from .models import Produto, cliente, Perfil
 
 class ProdutoAdm(admin.ModelAdmin):
     list_display = ('nome', 'preco', 'qtde')
 
+admin.site.register(Produto, ProdutoAdm)
 admin.site.register(Produto, ProdutoAdm)
 
--- aplicacao/models.py
+++ aplicacao/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phone_field import PhoneField
 
 class Produto(models.Model):
     nome = models.CharField("Nome", max_length=200, null = True)
@@ -6,3 +7,16 @@ class Produto(models.Model):
     qtde = models.PositiveIntegerField("Quantidade", default=0, null = True)
     def __str__(self):
         return self.nome
+
+class cliente(models.Model):
+    nome = models;charField("Nome", mas_length=200)
+    email = models.EmailField("Email", unique=True)
+    def __str__(self):
+        return self.nome
+    
+class Perfil(models.Model):
+    cliente = models.OneToOneField(cliente, on_delete=models.CASCADE, related_name='perfil')
+    telefone = models.CharField("Telefone", max_length=20)
+    rua = models.CharField("Rua", max_length=200)
+    numero = models.PositiveBigIntegerField("Nº", max_length=10)
+    cep = models.charField("Nº", max_length=200)
