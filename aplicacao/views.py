33ad35bee7148ec700748caeedc257e26422148b
DouglasAbmael <141178215+DouglasAbmael@users.noreply.github.com>
2025-09-01 14:55:26 -0300

    0109-4

--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Produto
 from django.http.response import HttpResponse
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+
+# Create your views here.
 
 def index(request):
     context = {
@@ -8,13 +12,66 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
-def produto(request):
+def produtos(request):
     produtos = Produto.objects.all()
-    # produtos = get_objects_or_404()
     context = {
-        'produtos': produtos,
+        'produtos' : produtos,
     }
-    return render(request, 'produto.html', context)
+    return render(request, 'produtos.html', context)
 
 def cad_produto(request):
-    return HttpResponse("tela de cadastro de produto")
+
+    if request.method == "GET":
+        return render(request, 'cad_produto.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        preco = request.POST.get('preco').replace(',', '.')
+        qtde = request.POST.get('qtde')
+
+        produto = Produto(
+            nome = nome,
+            preco = preco,
+            qtde = qtde
+        )
+        produto.save()
+        return redirect('url_produtos')
+    
+def atualizar_produto(request, id):
+    #prod = Produto.objects.get(id=id)
+    prod = get_object_or_404(Produto, id=id)
+    if request.method == "GET":
+        context = {
+            'prod': prod,
+        }
+        return render(request, 'atualizar_produto.html', context)
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        preco = request.POST.get('preco').replace(',', '.')
+        qtde = request.POST.get('qtde')
+
+        prod.nome = nome
+        prod.preco = preco
+        prod.qtde = qtde
+        prod.save()
+
+    return redirect('url_produtos')
+
+def apagar_produto(request, id):
+    prod = get_object_or_404(Produto, id=id)
+    prod.delete()
+    return redirect('url_produtos')
+
+def entrar(request):
+    if request.method == "GET":
+        return render(request, 'entrar.html')
+    else:
+        username = request.POST.get('nome')
+        password = request.POST.get('senha')
+        user = authenticate(username = username, password = password)
+
+        if user:
+            login(request, user)
+            return HttpResponse("Usuário logado com sucesso")
+        else:
+            return HttpResponse("Falha no login")
+
