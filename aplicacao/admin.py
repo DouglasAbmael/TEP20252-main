708bb6c3051bc8ecff9d0c440b35dae1d4d225ca
DouglasAbmael <141178215+DouglasAbmael@users.noreply.github.com>
2025-09-29 22:46:01 -0300

    atlzadmin

--- aplicacao/admin.py
+++ aplicacao/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Produto, Cliente, Perfil
+from .models import Venda, ItemVenda
 
 class ProdutoAdm(admin.ModelAdmin):
     list_display = ('nome', 'preco', 'qtde')
 
 admin.site.register(Produto, ProdutoAdm)
 admin.site.register(Cliente)
-admin.site.register(Perfil)
-
+admin.site.register(Venda)
+admin.site.register(ItemVenda)
